7444505c67105e2a28b91aa2b8b96d56d063956c
Roullito <jmoleins@laposte.net>
2025-02-18 16:06:59 +0100

    release one putchar to have only 4

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,16 +11,12 @@ int main(void)
 
 	while (single_digit_number <= '9')
 	{
+		putchar(single_digit_number);
+
 		if (single_digit_number != '9')
 		{
-			putchar(single_digit_number);
 			putchar(',');
 			putchar(' ');
-		}
-
-		else
-		{
-			putchar(single_digit_number);
 		}
 
 	single_digit_number++;
