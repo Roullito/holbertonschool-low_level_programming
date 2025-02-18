e2d2deb67304b3bd7903a270f8612dbd765d8c7e
Roullito <jmoleins@laposte.net>
2025-02-18 10:48:59 +0100

    change for match betty style

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,17 +22,17 @@ int main(void)
 
 	if (last_digit_of_n > 5)
 	{
-	printf("and is greater than 5 \n");
+	printf("and is greater than 5\n");
 	}
 
 	else if (last_digit_of_n < 6 || last_digit_of_n != 0)
 	{
-	printf("and is less than 6 and not 0 \n");
+	printf("and is less than 6 and not 0\n");
 	}
 
 	else if (last_digit_of_n == 0)
 	{
-	printf("and is 0 \n");
+	printf("and is 0\n");
 	}
 
 	return (0);
