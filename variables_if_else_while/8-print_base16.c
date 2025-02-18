75afa94d83bb9064a8e1ae4fbd6b1d300cad5d63
Roullito <jmoleins@laposte.net>
2025-02-18 15:45:33 +0100

    betty acceptation

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,20 +9,19 @@ int main(void)
 {
 	char hexa_number = '0';
 
-	while (hexa_number <= '9')
+		while (hexa_number <= '9')
 		{
 			putchar(hexa_number);
 			hexa_number++;
 		}
 	char hexa_lettre = 'a';
 
-	while (hexa_lettre <= 'f')
+		while (hexa_lettre <= 'f')
 		{
 			putchar(hexa_lettre);
 			hexa_lettre++;
 		}
-
-	putchar('\n');
+putchar('\n');
 
 	return (0);
 }
