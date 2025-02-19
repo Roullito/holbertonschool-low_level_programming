ddabf8828a38da4e0c11f010684e1e6fea1cd55d
Roullito <jmoleins@laposte.net>
2025-02-19 15:13:12 +0100

    ajoute des commentaire pour betty style

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * _islower() - checks for lowercase character. 
- * Return: Returns 1 if it's lowercase
+ * _islower - checks for lowercase character.
+ * Return: (return 1) (return 0)
+ * Returns 1 if it's lowercase
  * Returns 0 otherwise
+ * @c : C'est un paramètre local.
+ * Il reçoit une copie de l'argument lors de l'appel ('H', 'o', 108)
+ * Il est indépendant de r.
  */
 
 int _islower(int c)
+
 {
 	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		(return 1);
 	}
 
 	else
 	{
-		return 0;
+		(return 0);
 	}
 }
