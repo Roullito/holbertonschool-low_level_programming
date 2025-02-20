f85949c113b9620c239b9a230d3cd150218725c5
Roullito <jmoleins@laposte.net>
2025-02-20 11:18:38 +0100

    betty style

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 
 /**
- * _abs -  computes  the absolute value of the integer argument.
- * @j : variable qui doit etre absolue.
+ * _abs - Computes the absolute value of an integer.
+ * @j: The integer to compute the absolute value for.
+ *
+ * Return: The absolute value of the integer j.
  */
 
 int _abs(int j)
+
 {
 	if (j < 0)
 	{
-		j = j/-1;
-		return j;
+		j = j / -1;
+		return (j);
 	}
 
 	else
 	{
-		return j;
+		return (j);
 	}
 }
-
-	
