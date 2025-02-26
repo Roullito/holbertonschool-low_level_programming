784b4005d8d6c7b1959c80a09e2ea7fb9c89f8de
Roullito <jmoleins@laposte.net>
2025-02-26 15:33:02 +0100

    ajout de find == 1 pour que le code s'arrette après les premiers chiffres soient trouvé

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,19 +13,25 @@ int _atoi(char *s)
 	int i;
 	int result = 0;
 	int sign = 1;
+	int find = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
+			find = 1;
 		}
 
-		else if (s[i] == '-')
+		else if (s[i] == '-' && find == 0)
 		{
 			sign = sign * -1;
 		}
 
+		else if (find == 1)
+			{
+				break;
+			}
 	}
 return (result * sign);
 }
