be68111dca9b7ca587703f812c3bee63c82e1ead
Roullito <jmoleins@laposte.net>
2025-02-26 09:33:12 +0100

    function that reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+ /**
+ * print_rev - function that prints a string in reverse
+ *
+ * @s: address of charactere of the string
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int index1 = 0;
+	int index2 = 0;
+	int temp = 0;
+
+	while (s[index1] != '\0')
+	{
+		index1++;
+	}
+
+	while (index2 < index1)
+	{
+		index1--;
+		temp = s[index1];
+		s[index1] = s[index2];
+		s[index2] = temp;
+		index2++;
+	}
+}
