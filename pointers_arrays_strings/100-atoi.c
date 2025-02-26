a06efbec39060c2534908ee8d00a3627ef85a263
Roullito <jmoleins@laposte.net>
2025-02-26 15:45:40 +0100

    changement pour eviter INT_MIN

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -17,13 +17,7 @@ int _atoi(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-			find = 1;
-		}
-
-		else if (s[i] == '-' && find == 0)
+		if (s[i] == '-' && find == 0)
 		{
 			sign = sign * -1;
 		}
@@ -31,6 +25,12 @@ int _atoi(char *s)
 		else if (s[i] == '+' && find == 0)
 		{
 
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			find = 1;
 		}
 
 		else if (find == 1)
