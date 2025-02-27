6bbf96754f76db1851c78099285c412839c69318
Roullito <jmoleins@laposte.net>
2025-02-27 19:37:21 +0100

    changement du tableau temporaire car non géré par le checkeur.

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -11,16 +11,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int temp[n];
+	int temp;
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		temp[i] = a[n - 1 - i];
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
 
-	for (i = 0; i < n; i++)
-	{
-		a[i] = temp[i];
-	}
 }
