b820f6ea569feee14974ade35e34c5f58e61d608
Roullito <jmoleins@laposte.net>
2025-03-03 16:14:59 +0100

    betty

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,28 @@
 #include "main.h"
 
 /**
- *
- */
+* _strchr - function that locates a character in a string.
+* @s: pointeur vers la chaine de caractère à tester
+* @c: charactère à localiser
+* Return: la chaine à partir de c et retourn 0 si rien est trouvé
+*/
 
- char *_strchr(char *s, char c)
- {
+	char *_strchr(char *s, char c)
+	{
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
+	if (s[i] == c)
+	{
+	return (&s[i]);
+	}
 	}
 
 	if (c == '\0')
 	{
-		return (&s[i]);
+	return (&s[i]);
 	}
 
 	return (0);
- }
+	}
