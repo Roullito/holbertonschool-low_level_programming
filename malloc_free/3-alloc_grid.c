0c5ffd63765a5d66fc8d09c067e9468c82846a62
Roullito <jmoleins@laposte.net>
2025-03-10 16:50:32 +0100

    ajout d'une fonction free grid pour libérer l'espace

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -9,38 +7,49 @@
  *
  * Return: Pointer to the allocated grid, or NULL if failure.
  */
-
 int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int i, j;
 
-	if (width <= 0)
-		return (NULL);
-
-	if (height <= 0)
-		return (NULL);
+	if (width <= 0 || height <= 0)
+	return (NULL);
 
 	grid = malloc(height * sizeof(int *));
-
 	if (grid == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(width * sizeof(int));
 
-		if (grid[i] == NULL)
-		{
-			free(grid);
-			return (NULL);
-		}
+	grid[i] = malloc(width * sizeof(int));
 
+	if (grid[i] == NULL)
+	{
+		free_grid(grid, i);
+		return (NULL);
+	}
 		for (j = 0; j < width; j++)
-		{
-			grid[i][j] = 0;
-		}
+		grid[i][j] = 0;
 	}
 
 	return (grid);
+}
+
+/**
+ * free_grid - Frees a 2D grid created by alloc_grid.
+ * @grid: The 2D grid.
+ * @height: The number of rows.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
 }
