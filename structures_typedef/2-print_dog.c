3b1ecc8ccdbe7b6d2a60da5894f01f5491dbc888
Roullito <jmoleins@laposte.net>
2025-03-12 11:43:14 +0100

    supression de if age == 0 car age peut etre egale à 0 et NULL ne fonctionne pas sur un float. ajout de owner: et name: si ils sont = NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,14 +18,10 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	if (d->age == 0)
-		printf("(nil)\n");
-
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("(nil)\n");
+		printf("Owner: (nil)\n");
 
 	else
 		printf("Owner: %s\n", d->owner);
