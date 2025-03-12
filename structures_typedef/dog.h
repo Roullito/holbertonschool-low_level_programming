b20ba18d3948bc8a7f0efc1a975d471c31bd0d11
Roullito <jmoleins@laposte.net>
2025-03-12 10:30:32 +0100

    betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure de 3 infos
+ * @name: pointeur vers un char
+ * @age: chiffre à virgule
+ * @owner: pointeur vers un char
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
