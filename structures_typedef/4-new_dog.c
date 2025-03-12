03a284b6917b27d34ae8f5c1277cd6a0501f6ff7
Roullito <jmoleins@laposte.net>
2025-03-12 12:44:03 +0100

    code trop long donc création d'une nouvelle fonction pour l'allocation et la copie

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,6 +4,33 @@
 #include "dog.h"
 
 /**
+ * copy_string - Allocates memory and copies a string
+ * @str: The string to copy
+ * Return: Pointer to the new string, or NULL if it fails
+ */
+char *copy_string(char *str)
+{
+	char *new_str;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	new_str = malloc(len + 1);
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		new_str[i] = str[i];
+
+	new_str[len] = '\0';
+	return (new_str);
+}
+
+/**
  * new_dog - Creates a new dog
  * @name: Name of the dog
  * @age: Age of the dog
@@ -14,49 +41,30 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int i, j;
-	int len_name = 0;
-	int len_owner = 0;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	while (name[len_name] != '\0')
-		len_name++;
-
-	new_dog->name = malloc(len_name + 1);
+	new_dog->name = copy_string(name);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	for (i = 0; name[i] != '\0'; i++)
-	{
-		new_dog->name[i] = name[i];
-	}
-	new_dog->name[i] = '\0';
-
 	new_dog->age = age;
 
-	while (owner[len_owner] != '\0')
-		len_owner++;
-
-	new_dog->owner = malloc(len_owner + 1);
+	new_dog->owner = copy_string(owner);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-
-	for (j = 0; owner[j] != '\0'; j++)
-	{
-		new_dog->owner[j] = owner[j];
-	}
-	new_dog->owner[j] = '\0';
 
 	return (new_dog);
 }
