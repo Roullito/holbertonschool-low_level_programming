201d5cb9295c3dd226f626be0463e34e8840feee
Roullito <jmoleins@laposte.net>
2025-03-17 12:08:44 +0100

    function that searches for an integer.

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer.
+ * @array: pointeur vers un tableau
+ * @size: taille du tableau
+ * @cmp: pointeur vers une fonction
+ * Return: i si match -1 si non
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
