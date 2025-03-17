97cb769bd0d12f896e40f050e676fd93b42e2702
Roullito <jmoleins@laposte.net>
2025-03-17 11:07:11 +0100

    betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name -  function that prints a name.
+ * @name: pointeur vers un char (Bob et Bob dylan)
+ * @f: pointeur vers une fonction (as_is et upper)
+ * Return: void
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
