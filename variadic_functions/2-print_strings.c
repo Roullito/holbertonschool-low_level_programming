cf0ae547da2756aa9c0f6e1bc92575afd7349154
Roullito <jmoleins@laposte.net>
2025-03-18 15:50:28 +0100

    ajout de nil if string == NULL

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,16 +22,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
+		string = va_arg(args, char *);
+
+		if (string == NULL)
+			printf("(nil)");
+
 		if (i != (n - 1))
 		{
-			string = va_arg(args, char *);
 			printf("%s", string);
 			printf("%s", separator);
 		}
 
 		else
 		{
-			string = va_arg(args, char *);
 			printf("%s", string);
 		}
 	}
