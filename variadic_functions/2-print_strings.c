a904771a46a99f0469fd23b4a2f53c3e53f8a9ac
Roullito <jmoleins@laposte.net>
2025-03-18 15:52:08 +0100

    enlever la fonction if separator == NULL

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,9 +17,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-		separator = "";
-
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(args, char *);
