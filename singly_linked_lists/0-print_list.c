6b5642fc85680805fce968e75d87e7386cfceeb9
Roullito <jmoleins@laposte.net>
2025-03-31 14:26:30 +0200

    betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * print_list - Function that prints all the elements of a list_t list.
+ * @h: pointer to the list
+ * Return: i
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
@@ -9,16 +15,16 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf ("[0] (nil)\n");
+			printf("[0] (nil)\n");
 
 		else
 		{
-			printf ("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 
 		i++;
 		h = h->next;
 	}
 
-	return(i);
+	return (i);
 }
