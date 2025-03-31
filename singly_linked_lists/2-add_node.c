c8d0671d533e7cce7859cf29a92ec3060fd82d18
Roullito <jmoleins@laposte.net>
2025-03-31 15:21:13 +0200

    strlen not allowed so i used loop

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,8 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	int i;
+
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
@@ -25,7 +27,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		new_node->len = i;
+	}
+
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
