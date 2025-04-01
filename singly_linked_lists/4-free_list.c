a8e4424273183fb9cf75cfe0f5fcfcc994dd8f20
Roullito <jmoleins@laposte.net>
2025-04-01 10:05:27 +0200

    betty

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * free_list - Function that frees a list_t list.
+ * @head: pointer to the beggining of list_t
+ * Return: void
+ */
+
 void free_list(list_t *head)
 {
 	list_t *temp;
